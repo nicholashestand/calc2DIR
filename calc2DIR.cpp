208ab2fa67270238d9cb10c7937e24b8ec7193b9
Nicholas Hestand <nicholasjhestand@gmail.com>
2019-01-05 14:55:13 -0600

    calc2DIR:: add linear spectrum calculation

--- calc2DIR.cpp
+++ calc2DIR.cpp
@@ -3,12 +3,11 @@
 #include <sstream>
 #include <iostream>
 #include <stdio.h>
+#include <complex.h>
 #include <fftw3.h>
 #include <math.h>
 #include <algorithm>
 #include "calc2DIR.h"
-
-#define PI M_PI
 
 using namespace std;
 
@@ -25,8 +24,12 @@ IR2D::IR2D( string _inpf_ )
     }
 
     // allocate variables for energy and dipole
-    energy = new float[nchrom]();
-    dipole = new vec3[nchrom]();
+    energy          = new double[nchrom]();
+    energy_last     = new double[nchrom]();
+    dipole          = new vec3[nchrom]();
+    dipole0         = new vec3[nchrom]();
+    R1D             = new complex<double>[R1D_npoints]();
+    egt             = new complex<double>[nchrom]();
 
     // open the energy and dipole files
     efile.open( efn, ios::binary );    
@@ -41,7 +44,11 @@ IR2D::~IR2D()
 {
     // destroy arrays
     delete [] energy;
+    delete [] energy_last;
     delete [] dipole;
+    delete [] dipole0;
+    delete [] R1D;
+    delete [] egt;
 
     // close files
     efile.close();
@@ -69,19 +76,21 @@ int IR2D::readParam( string _inpf_ )
 
         // transform parameter name to lower case
         transform( para.begin(), para.end(), para.begin(), ::tolower);
-        //cout << para << endl;
 
         // save parameters as class variable
         if      ( para.compare("energy_file") == 0 )  efn          = value;
         else if ( para.compare("dipole_file") == 0 )  dfn          = value;
+        else if ( para.compare("output_file") == 0 )  ofn          = value;
         else if ( para.compare("time_step") == 0 )    dt           = stof(value);
         else if ( para.compare("t1_max") == 0 )       t1_max       = stof(value);
         else if ( para.compare("t3_max") == 0 )       t3_max       = stof(value);
         else if ( para.compare("t2") == 0 )           t2           = stof(value);
+        else if ( para.compare("rt2") == 0 )          RT2          = stof(value);
         else if ( para.compare("nsamples") == 0 )     nsamples     = stoi(value);
         else if ( para.compare("sample_every") == 0 ) sample_every = stof(value);
-        else if ( para.compare("nframes") == 0 )      nframes      = stoi(value);
         else if ( para.compare("nchrom") == 0 )       nchrom       = stoi(value);
+        else if ( para.compare("window0") == 0 )      window0      = stof(value);
+        else if ( para.compare("window1") == 0 )      window1      = stof(value);
         else cerr << "\tWARNING:: parameter " << para << " is not recognized." << endl;
     }
     inpf.close();
@@ -89,15 +98,25 @@ int IR2D::readParam( string _inpf_ )
     // some output to confirm parameters
     cout << "\tSetting energy_file to: " << efn << endl;
     cout << "\tSetting dipole_file to: " << dfn << endl;
+    cout << "\tSetting output_file to: " << ofn << endl;
     cout << "\tSetting time_step to: " << dt << " ps" << endl;
     cout << "\tSetting t1_max to: " << t1_max << " ps" << endl;
     cout << "\tSetting t3_max to: " << t3_max << " ps" << endl;
     cout << "\tSetting t2 to: " << t2 << " ps" << endl;
+    cout << "\tSetting relaxation time T2 to: " << RT2 << " ps" << endl;
     cout << "\tSetting nsamples to: " << nsamples << endl;
     cout << "\tSetting sample_every to: " << sample_every << " ps" << endl;
-    cout << "\tSetting nframes to: " << nframes << endl;
     cout << "\tSetting nchrom to: " << nchrom << endl;
+    cout << "\tSetting spectral limits to: " << window0 << " - " << window1 << " cm." << endl;
     cout << ">>> Done reading simulation parameters from " << _inpf_ << endl;
+
+    // set number of points in response functions based on t1_max and dt
+    R1D_npoints = static_cast<int>(t1_max/dt);
+
+    // shift energies by mean of spectral window to avoid high frequency oscillations
+    shift = (window1 + window0)/2.;
+    cout << ">>> Shifting frequencies by: " << shift << " cm." << endl;
+
 
     return IR2DOK;
 }
@@ -114,20 +133,21 @@ int IR2D::readEframe( int frame )
     int frameTmp; 
     float energyTmp[ nchrom*(nchrom+1)/2 ]; 
     int64_t file_offset;
-                                            
+
     file_offset = frame*(sizeof(int)+sizeof(float)*nchrom*(nchrom+1)/2);
     efile.seekg( file_offset );
 
     efile.read( (char*)&frameTmp, sizeof(int) );
     efile.read( (char*)energyTmp, sizeof(float)*nchrom*(nchrom+1)/2 );
-    return 1;
 
     // only keep the energies, ignore the couplings for now
     int col=0;
     for ( int i = 0; i < nchrom; i ++ ){
-        energy[nchrom + i] = energyTmp[col];
+        energy[i] = energyTmp[col] - shift;
         col += nchrom-i;
     }
+
+    return IR2DOK;
 }
 
 int IR2D::readDframe( int frame )
@@ -150,11 +170,129 @@ int IR2D::readDframe( int frame )
             dipole[chrom][i] = dipoleTmp[i*nchrom + chrom];
         }
     }
+
+    return IR2DOK;
+}
+
+complex<double> IR2D::getR1D( int t1 )
+// return the linear response function at a given t1 for a given chromophore
+// See Eq 7.10 from Hamm and Zanni
+{
+    double mu[nchrom];
+    complex<double> arg;
+    complex<double> R1Dtmp;
+
+    // reset all integrals to zero
+    if ( t1 == 0 ){
+        //   initialize egt to one
+        for ( int chrom = 0; chrom < nchrom; chrom ++ ) egt[chrom] = complex_one;
+    }
+    else {
+        for ( int chrom = 0; chrom < nchrom; chrom ++ ){
+            // integrate the equation for each chromophore using the trapezoid rule
+            arg = -img*dt*(energy_last[chrom]+energy[chrom])/(2.*HBAR);
+            egt[chrom] *= exp(arg);
+        }
+    }
+    
+    // get dipole part
+    for ( int chrom = 0; chrom < nchrom; chrom ++ ){
+        mu[chrom] = dot3(dipole0[chrom],dipole[chrom]);
+    }
+
+    // return the response function
+    // average the response function over all of the chromophores then return
+    R1Dtmp = complex_zero;
+    for ( int chrom = 0; chrom < nchrom; chrom ++ ){
+        R1Dtmp += img*mu[chrom]*egt[chrom]/(1.*nchrom);
+    }
+
+    return R1Dtmp;
+}
+
+double IR2D::dot3( vec3 x, vec3 y )
+// dot product of 3 vector
+{
+    return x[0]*y[0] + x[1]*y[1] + x[2]*y[2]; 
+}
+
+int IR2D::write1D()
+// write response function and FTIR spectrum
+{
+    string fn;
+    ofstream ofile;
+    const int length=32768;
+    complex<double> fftIn[length];
+    complex<double> fftOut[length];
+    fftw_plan plan;
+    double convert, freq, intens, abs[length], dis[length];
+
+    fn = ofn+"-R1D.dat";
+    ofile.open(fn);
+    if ( ! ofile.is_open() ) { fileOpenErr( fn ); exit(EXIT_FAILURE);}
+    cout << ">>> Writing " << fn << "." << endl;
+
+    ofile << "# time (ps) Real Imag" << endl;
+    for ( int i = 0; i < R1D_npoints; i ++ ){
+        ofile << i * dt << " " << R1D[i].real() << " " << R1D[i].imag() << endl;
+    }
+    ofile.close();
+
+    // do fft
+    plan = fftw_plan_dft_1d( length, reinterpret_cast<fftw_complex*>(fftIn), \
+                                     reinterpret_cast<fftw_complex*>(fftOut),\
+                                     FFTW_BACKWARD, FFTW_ESTIMATE );
+    
+    // See Eq 4.8 from Hamm and Zanni -- Absorptive part
+    for ( int i = 0; i < length ; i ++ ) fftIn[i] = complex_zero;
+    for ( int i = 0; i < R1D_npoints; i ++ ){
+        fftIn[i]        = img*R1D[i]*exp(-i*dt/RT2);
+        fftIn[length-i] = conj(img*R1D[i])*exp(-i*dt/RT2);
+    }
+    fftw_execute(plan);
+    for ( int i = 0; i < length; i ++ ) abs[i] = fftOut[i].real();
+
+    // See Eq 4.11 from Hamm and Zanni -- Dispersive part
+    for ( int i = 0; i < length ; i ++ ) fftIn[i] = complex_zero;
+    for ( int i = 0; i < R1D_npoints; i ++ ){
+        fftIn[i]        = -R1D[i]*exp(-i*dt/RT2);
+        fftIn[length-i] = conj(-R1D[i])*exp(-i*dt/RT2);
+    }
+    fftw_execute(plan);
+    for ( int i = 0; i < length; i ++ ) dis[i] = fftOut[i].real();
+
+    fn = ofn+"-FTIR.dat";
+    ofile.open(fn);
+    if ( ! ofile.is_open() ) { fileOpenErr( fn ); exit(EXIT_FAILURE);}
+    cout << ">>> Writing " << fn << "." << endl;
+    ofile << "# frequency (cm-1) Absorption Dispersion" << endl;
+
+
+    intens = -dt*length/(2.*PI*HBAR*sqrt(length));
+    // scale intensity to keep total area of the spectrum conserved and normalize
+    // by root(length) since FFTW3 does not
+    // multiply the signal by -1 so it looks like absorption and not signal intensity
+        
+    // negative frequencies are stored at the end of the fftOut array
+    for ( int i = length/2; i < length; i ++ )
+    {
+        // get frequency in wavenumber
+        freq   = 2.*PI*HBAR*(i-length)/(dt*length) + shift;
+        if ( freq < window0 or freq > window1 ) continue;
+        ofile << freq << " " << intens*abs[i] << " " << intens*dis[i] << endl;
+    }
+    for ( int i = 0; i < length/2; i ++ ){
+        // get frequency in wavenumber
+        freq   = 2.*PI*HBAR*i/(dt*length) + shift;
+        if ( freq < window0 or freq > window1 ) continue;
+        ofile << freq << " " << intens*abs[i] << " " << intens*dis[i] << endl;
+    }
+    ofile.close();
+
 }
 
 int main( int argc, char* argv[] )
 {
-
     if ( argc != 2 ){
         cout << "ERROR:: Program expects the name of the input" << 
                 "file as the only argument. Aborting." << endl;
@@ -168,11 +306,34 @@ int main( int argc, char* argv[] )
     IR2D spectrum( inpf );
 
     // Loop over the trajectory
-    for ( int frame = 0; frame < spectrum.nframes; frame ++ )
-    {
-        // read in energy and dipole
-        spectrum.readEframe(frame);
-        spectrum.readDframe(frame);
+    for ( int sample = 0; sample < spectrum.nsamples; sample ++ ){
+        int frame0 = static_cast<int>(spectrum.sample_every/spectrum.dt);
+        // read and save dipole at t0
+        spectrum.readDframe(frame0);
+        memcpy( spectrum.dipole0, spectrum.dipole, spectrum.nchrom*3*sizeof(double) );
+        
+        // 1D spectrum -- t1 is in units of dt
+        for ( int t1 = 0; t1 < spectrum.R1D_npoints; t1 ++ )
+        {
+            // get frame number for current t1
+            int frame = frame0 + t1;
+
+            // read in energy and dipole
+            spectrum.readEframe(frame);
+            spectrum.readDframe(frame);
+            
+            spectrum.R1D[t1] += spectrum.getR1D( t1 );
+
+            // save energy for next
+            memcpy( spectrum.energy_last, spectrum.energy, spectrum.nchrom*sizeof(double) );
+        }
     }
 
+    // normalize r1d by number of samples
+    for ( int t1 = 0; t1 < spectrum.R1D_npoints; t1 ++ ){
+        spectrum.R1D[t1]/=(1.*spectrum.nsamples);
+    }
+
+    // write the response functions and the spectrum
+    spectrum.write1D();
 }
--- calc2DIR.h
+++ calc2DIR.h
@@ -2,29 +2,48 @@
 #include <complex.h>
 #include <fstream>
 
-typedef float vec3[3];
+typedef double vec3[3];
 using namespace std;
 
 #ifndef calc2DIR_H
 #define calc2DIR_H
 #define IR2DOK 0
+#define PI M_PI
+#define HBAR 5.308837367 // in cm-1*ps
 class IR2D
 {
     public:
         // variables
         string efn="e.dat";             // name for energy file
         string dfn="d.dat";             // name for dipole file
-        float  dt = 0.010 ;             // time step in ps
-        float  t1_max=10. ;             // t1 max in ps
-        float  t2    =0.  ;             // t2 in ps
-        float  t3_max=10. ;             // t3 max in ps
+        string ofn="spectrum";          // name for output file
+        double dt = 0.010 ;             // time step in ps
+        double t1_max=10. ;             // t1 max in ps
+        double t2    =0.  ;             // t2 in ps
+        double t3_max=10. ;             // t3 max in ps
+        double RT2=0.3;                 // T2 relaxation time
         int    nsamples=1 ;             // number of samples
-        float  sample_every=10.;        // how often to take a new sample in ps
-        int    nframes=0;               // number of frames in energy and dipole files to consider
+        double sample_every=10.;        // how often to take a new sample in ps
         int    nchrom=1;                // number of uncoupled chromophores to consider
+        int    R1D_npoints;             // number of data points in 1D response function
+        double window0 = 1400;          // lower limit of spectral window in cm-1
+        double window1 = 1700;          // upper limit if spectral window in cm-1
+        double shift;                   // reference frequency in cm-1
 
-        float *energy;                  // energy
-        vec3  *dipole;                  // dipole vector
+        // arrays to hold energy and dipole
+        double *energy;                  // energy
+        double *energy_last;             // energy from previous frame
+        vec3   *dipole;                  // dipole vector
+        vec3   *dipole0;                 // dipole vector at t0
+
+        // constants
+        const complex<double> img          = {0.,1.};   
+        const complex<double> complex_one  = {1.,0.};   
+        const complex<double> complex_zero = {0.,0.};
+      
+        // response functions
+        complex<double> *R1D;            // Linear response function
+        complex<double> *egt;            // to keep track of the lineshape g(t)
 
         // file handles
         ifstream efile, dfile;
@@ -38,6 +57,9 @@ class IR2D
         int readParam( string _inpf_ );
         int readEframe( int frame );
         int readDframe( int frame );
+        complex<double> getR1D( int t1 );
+        int write1D();
 
+        double dot3( vec3 x, vec3 y );
 };
 #endif
